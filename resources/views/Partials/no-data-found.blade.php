a20848f68fbfb07538be97dc965c103eee54fa8f
Nguyen Duc Thang <75838800+nguyenthang327@users.noreply.github.com>
2023-07-15 09:18:54 +0700

    Delete resources/views/Partials directory

--- resources/views/Partials/no-data-found.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="no-data-found">
-    {{-- <img class="no-data-found__img" src="{{asset("images/no-data-found.png")}}" alt=""> --}}
-    <h5 class="no-data-found__title">{{trans('message.nothing_data')}}</h5>
-</div>
