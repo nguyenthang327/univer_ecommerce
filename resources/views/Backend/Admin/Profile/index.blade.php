2b241a9f9143795636559192feb709526f49f2e7
Nguyen Duc Thang <75838800+nguyenthang327@users.noreply.github.com>
2023-07-15 09:19:17 +0700

    Delete resources/views/Backend directory

--- resources/views/Backend/Admin/Profile/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('backend.admin.layout.master')
-@section('title',trans('language.dashboard'))
-
-@section('content')
-    
-@stop
--- resources/views/Backend/user/profile/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('backend.user.layout.master')
-@section('title',trans('language.dashboard'))
-
-@section('content')
-    
-@stop
